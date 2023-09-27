2c89f6eb8dc2394416a0a9fd28e117ba858b9bf1
Op-Pautu <pautuop@gmail.com>
2023-09-27 15:51:45 +0530

    Install zustand and add shadcn dialog comp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import Modal from "@/components/ui/modal";
 
 export default function SetupPage() {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal title="test" description="test desc" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 }
