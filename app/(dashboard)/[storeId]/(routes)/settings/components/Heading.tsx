a3d2f18c3721e94c6ab9f7905205bf9267067bfa
Op-Pautu <pautuop@gmail.com>
2023-09-28 19:58:47 +0530

    Add form from shadcn

--- app/(dashboard)/[storeId]/(routes)/settings/components/Heading.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/Heading.tsx
@@ -9,7 +9,7 @@ const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
       <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-      <p className="text-sm text-muted-foreground">{description}</p>
+      <p className="text-sm text-muted-foregroundq">{description}</p>
     </div>
   );
 };
