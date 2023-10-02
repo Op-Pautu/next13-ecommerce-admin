323528154a65f8950dd518bff5fcac12fbca17ef
Op-Pautu <pautuop@gmail.com>
2023-10-02 13:09:34 +0530

    Add UI for orders page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/dataTable";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable searchKey="products" columns={columns} data={data} />
+    </>
+  );
+};
